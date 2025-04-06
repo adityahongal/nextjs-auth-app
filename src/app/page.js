8323b34d483a309dc449b83cb218bd43fcea9295
Aditya Hongal <adityahongalwork@gmail.com>
2025-04-06 22:10:54 +0530

    App Setup

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-indigo-600">
+        Tailwind v4 + App Router working! 🚀
+      </h1>
+    </div>
+    </div>
+  );
+}
